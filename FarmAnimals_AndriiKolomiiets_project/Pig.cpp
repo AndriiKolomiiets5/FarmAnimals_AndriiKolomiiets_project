a95f1e77404fa07f8ccc3381dc81b305f07f37d4
Andrii Kolomiiets <UL0289937@edu.uni.lodz.pl>
2024-06-22 17:52:30 +0200

    Realization of the class

--- FarmAnimals_AndriiKolomiiets_project/Pig.cpp
+++ FarmAnimals_AndriiKolomiiets_project/Pig.cpp
@@ -13,19 +13,3 @@ std::string Pig::Breed() {
 }
 std::string Pig::Size() {
 	return "Small";
-#include "Pig.h"
-	Pig::Pig(double _weight, double _age, double _HP, double _price) :
-		Animal(_weight, _age, _HP, _price) {}
-	std::string Pig::Name() {
-		return "Peppa";
-	}
-	std::string Pig::AnimalName()
-	{
-		return "Pig";
-	}
-	std::string Pig::Breed() {
-		return "White Big pig";
-	}
-	std::string Pig::Size() {
-		return "Small";
-	}
